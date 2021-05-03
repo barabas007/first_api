2789a75c4ee4d4c684de6f6024d1c19051e6408e
barabas007 <shop.ru.shop@mail.ru>
2021-05-03 14:11:46 +0300

    added validates :password, :email, :name

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  validates_format_of :email, with: URI::MailTo::EMAIL_REGEXP
+  validates :password, presence: true, length: { minimum: 6, maximum: 254 }
+  validates :name, presence: true
 end
