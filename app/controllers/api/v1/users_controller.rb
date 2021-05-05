c4044094340942c0d65edc028d4e5633a1f998dd
barabas007 <shop.ru.shop@mail.ru>
2021-05-05 22:04:05 +0300

    added create and destroy

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,13 +2,33 @@ module Api
   module V1
     class UsersController < ApplicationController
       has_secure_password
-      def index ;end
+      def index; end
 
       def show
         render json: User.all
       end
+
       def show_user
         render json: User.find(params[:id])
+      end
+
+      def create
+        user = User.new(user_params)
+
+        if user.save
+          render json: user, status: :created
+        else
+          render json: user.errors, status: :unprocessable_entity
+        end
+      end
+
+      def destroy
+        User.find(params[:id]).destroy!
+        head :no_content
+      end
+
+      def user_params
+        params.require(:user).permit(:name, :email, :password, :password_digest)
       end
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :users, only: %i[show show_user]
+      resources :users, only: %i[show show_user create]
       post 'login', to: 'login#create'
     end
   end
