7680e80733948a93e96adb4dd6a263dad562c773
barabas007 <shop.ru.shop@mail.ru>
2021-05-06 10:38:44 +0300

    changed validates :email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-  validates_format_of :email, with: URI::MailTo::EMAIL_REGEXP, presence: true
+  # validates_format_of :email, with: URI::MailTo::EMAIL_REGEXP, presence: true
+  validates :email, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :password, presence: true, length: { minimum: 6, maximum: 254 }
   validates :name, presence: true
 end
