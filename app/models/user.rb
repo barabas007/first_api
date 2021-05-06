5e2637cfe4bc2defe266a57e2bafab90141bcd0b
barabas007 <shop.ru.shop@mail.ru>
2021-05-06 16:38:19 +0300

    added validates :user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   # validates_format_of :email, with: URI::MailTo::EMAIL_REGEXP, presence: true
   has_secure_password
   has_secure_password :recovery_password, validations: false
+  has_one :user
+  validates :user, presence: true
   validates :email, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :password, presence: true, length: { minimum: 6, maximum: 254 }
   validates :name, presence: true
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
+def method_name
+  post 'login', to: 'login#create'
+  get 'users', to: 'users#show'
+  put 'users', to: 'users#edit'
+  delete 'users', to: 'users#destroy'
+  get 'users', to: 'users#show_user'
+  resources :users, only: %i[show show_user create edit]
+end
+
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
-    namespace :v1, defaults: { format: :json } do
-      post 'login', to: 'login#create'
-      get 'users', to: 'users#show'
-      put 'users', to: 'users#edit'
-      delete 'users', to: 'users#destroy'
-      get 'users', to: 'users#show_user'
-      resources :users, only: %i[show show_user create edit]
-    end
+    namespace :v1, defaults: {format: :json}, &method(:method_name)
   end
 end
