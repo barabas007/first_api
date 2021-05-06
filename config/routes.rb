1ef071e8dfea6b31cdb3ce3459267256de89971b
barabas007 <shop.ru.shop@mail.ru>
2021-05-06 10:45:15 +0300

    routes added defaults format: :json

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
-  namespace :api do
-    namespace :v1 do
+  namespace :api, defaults: { format: :json } do
+    namespace :v1, defaults: { format: :json } do
       resources :users, only: %i[show show_user create]
       post 'login', to: 'login#create'
     end
