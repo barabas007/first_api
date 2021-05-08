0f4f8c2d03b5809102cde32542893e60f4535611
barabas007 <shop.ru.shop@mail.ru>
2021-05-08 20:18:26 +0300

    added test plas routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 def method_user
   post 'login', to: 'login#create'
-  get 'users', to: 'users#show'
-  put 'users', to: 'users#edit'
-  delete 'users', to: 'users#destroy'
-  get 'users', to: 'users#show_user'
-  post 'users', to: 'users#login'
-  get 'users', to: 'users#index'
-  put 'users', to: 'user#edit'
+  get 'users', to: '/api/v1/users#show'
+  put 'users', to: '/api/v1/users#edit'
+  delete 'users', to: '/api/v1/users#destroy'
+  get 'users', to: '/api/v1/users#show_user'
+  post 'users', to: '/api/v1/users#login'
+  get 'users', to: '/api/v1/users#index'
+  put 'users', to: '/api/v1/user#edit'
+  post 'users', to: '/api/v1/users#create'
 
   resources :users, only: %i[show show_user create edit login index]
 end
